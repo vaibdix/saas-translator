b1e870d3474c8023d6d4a2a9878a16ea15e7a80b
vaibdix <dixitvaib@gmail.com>
2024-01-08 20:24:18 +0530

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Logo from './Logo'
+
+function Header() {
+  return (
+    <header className='sticky top-0 z-50 bg-white dark:bg-gray-900'>
+        <nav className='flex flex-col sm:flex-row items-center p-5 pt-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto'>
+            <Logo />
+            <div className='flex-1 flex items-center justify-end space-x-4'>
+              {/* lang select */}
+              {/*session && (
+                ...
+              )*/}
+
+              {/* dark mode toggle */}
+
+              {/* user button */}
+            </div>
+        </nav>
+        {/* upgradde banner */}
+    </header>
+  )
+}
+
+export default Header
