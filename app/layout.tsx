6c2d642dc89420b1f46c3da0d3e205cf2d2b1232
vaibdix <dixitvaib@gmail.com>
2024-01-10 01:55:51 +0530

    chore: udpates for auth & session

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header'
 import { ThemeProvider } from '@/components/ThemeProvider'
+import ClientProviders from '@/components/ClientProvides'
 // import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,18 +19,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider
+    <ClientProviders>
+      <html lang="en">
+        <body className={inter.className}>
+          <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
-            <Header/>
+            <Header />
             {children}
-        </ThemeProvider>
-      </body>
-    </html>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClientProviders>
   )
 }
--- components/UserButton.tsx
+++ components/UserButton.tsx
@@ -1,30 +1,45 @@
+"use client";
+
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import UserAvatar from "./UserAvatar"
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import UserAvatar from './UserAvatar'
+import { Session } from 'next-auth'
+import { Button } from './ui/button'
+import { signIn, signOut } from 'next-auth/react'
 
-function UserButton() {
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger>
-                <UserAvatar name="vaibhav" image="https://github.com/shadcn.png"/>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Billing</DropdownMenuItem>
-                <DropdownMenuItem>Team</DropdownMenuItem>
-                <DropdownMenuItem>Subscription</DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+function UserButton({ session }: { session: Session | null }) {
+  // Subscription listener ...
+  if (!session) return (
+    <Button variant={'outline'} onClick={() => signIn()}>
+      Sign In
+    </Button>
+  )
 
-    )
+
+  return session && (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <UserAvatar
+          name={session.user?.name}
+          image={session.user?.image}
+        />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>{session.user?.name}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {/* <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem> */}
+        <DropdownMenuItem onClick={() => signOut()}>Sign Out</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
 }
 
 export default UserButton
