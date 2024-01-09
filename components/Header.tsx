0fa8bd4a36efc5cd2d76e95469f24cfa228096f4
vaibdix <dixitvaib@gmail.com>
2024-01-10 01:56:26 +0530

    chore: basic button addition

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,23 +2,42 @@ import React from 'react'
 import Logo from './Logo'
 import DarkModeToggle from './DarkModeToggle'
 import UserButton from './UserButton'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/auth'
+import Link from 'next/link'
+import { MessageSquareIcon } from 'lucide-react'
+import CreateChatButton from './CreateChatButton'
 
-function Header() {
+async function Header() {
+
+  const session = await getServerSession(authOptions)
+  console.log(session)
+
   return (
     <header className='sticky top-0 z-50 bg-white dark:bg-gray-900'>
         <nav className='flex flex-col sm:flex-row items-center p-5 pt-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto'>
             <Logo />
             <div className='flex-1 flex items-center justify-end space-x-4'>
               {/* lang select */}
-              {/*session && (
-                ...
-              )*/}
+              {session ? (
+                <>
+                  <Link
+                    href={'/chat'} prefetch={false}>
+                    <MessageSquareIcon className='text-black dark:text-white' />
+                  </Link>
+                  <CreateChatButton />
+                </>
+              ): (
+                <Link href='/pricing'>
+                  Pricing
+                </Link>
+              )}
 
               {/* dark mode toggle */}
               <DarkModeToggle />
-              
+
               {/* user button */}
-              <UserButton />
+              <UserButton session={session} />
             </div>
         </nav>
         {/* upgradde banner */}
--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -1,31 +1,39 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { cn } from "@/lib/utils"
-import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
 
 function UserAvatar({
-      name,
-      image,
-      className
-  }: {
-      name: string;
-      image: string;
-      className?: string;
-  }) {
+  name,
+  image,
+  className
+}: {
+  name?: string | null
+  image?: string | null
+  className?: string
+}) {
   return (
-    <Avatar className={cn("bg-whitetext-black", className)}>
-        {image && (
-            <Image
-                src={image}
-                alt={name}
-                width={40}
-                height={40}
-                className="rounded-full"
-            />
-        )}
+    <Avatar className={cn('bg-whitetext-black', className)}>
+      {image && (
+        <Image
+          src={image}
+          alt={name || 'User Name'}
+          width={40}
+          height={40}
+          referrerPolicy='no-referrer'
+          className='rounded-full'
+        />
+      )}
 
-  <AvatarFallback>CN</AvatarFallback>
-</Avatar>
-
+      <AvatarFallback
+        delayMs={1000}
+        className='dark:bg-white dark:text-black text-lg'>
+        {name
+          ?.split(' ')
+          .map((n) => n[0])
+          .join('')
+        }
+      </AvatarFallback>
+    </Avatar>
   )
 }
 
